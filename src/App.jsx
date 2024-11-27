aa8ffbb0d97133cd3260f811616f127b79a08942
prinzprogrammernew <princeprogrammer73@gmail.com>
2024-11-28 04:14:12 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div class="p-4 bg-blue-500 text-white rounded-sm">
+        <h1 className='underline'>Vite + React</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
